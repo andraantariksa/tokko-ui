3d39276b87f32c31e739b4650a972851b830d0ed
Andra Antariksa <andra.antariksa@gmail.com>
2021-04-12 22:58:51 +0700

    Fix home products display and home bottom navigation navigation view

--- app/src/main/java/io/github/andraantariksa/tokko/ui/HomeFragment.kt
+++ app/src/main/java/io/github/andraantariksa/tokko/ui/HomeFragment.kt
@@ -1,22 +1,24 @@
 package io.github.andraantariksa.tokko.ui
 
+import android.R
+import android.content.res.Resources
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.GridLayoutManager
+import androidx.fragment.app.Fragment
 import io.github.andraantariksa.tokko.ProductsAdapter
 import io.github.andraantariksa.tokko.data.model.Product
 import io.github.andraantariksa.tokko.databinding.FragmentHomeBinding
+
 
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
         val view = binding.root
@@ -25,9 +27,18 @@ class HomeFragment : Fragment() {
             Product("Hoodie KW", 80f),
             Product("Shoes Flexible", 120f),
             Product("Shoes Flexible", 120f),
-            Product("Shoes Flexible", 120f))
+            Product("Shoes Flexible", 120f)
+        )
 
-        binding.recyclerViewTopSelling.layoutManager = GridLayoutManager(context, 2)
+        val spacingInPixels = (Resources.getSystem().displayMetrics.density * 15).toInt()
+        binding.recyclerViewTopSelling.addItemDecoration(
+            GridSpacingItemDecoration(
+                2,
+                spacingInPixels,
+                false,
+                0
+            )
+        )
         binding.recyclerViewTopSelling.adapter = ProductsAdapter(dummy)
 
         return view
